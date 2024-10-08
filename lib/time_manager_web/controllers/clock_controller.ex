94be383c8a83e58c058d182b74a45609a130aa0e
ratataque <ewanrimmely@outlook.fr>
2024-10-08 22:50:12 +0200

    clocks test pass

--- lib/time_manager_web/controllers/clock_controller.ex
+++ lib/time_manager_web/controllers/clock_controller.ex
@@ -12,12 +12,18 @@ defmodule TimeManagerWeb.ClockController do
     render(conn, :index, clocks: clocks)
   end
 
-  def create(conn, %{"clock" => clock_params}) do
+  def create(conn, %{"clock" => clock_params, "userID" => user_id}) do
+    # Merge the user_id into clock_params
+    clock_params = Map.put(clock_params, "user_id", user_id)
+
+    # Insert the clock into the database
     with {:ok, %Clock{} = clock} <- Timesheet.create_clock(clock_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", ~p"/api/clocks/#{clock}")
-      |> render(:show, clock: clock)
+      |> json(%{data: "created"})
+
+      # |> put_resp_header("location", ~p"/api/clocks/#{clock}")
+      # |> render(:show, clock: clock)
     end
   end
 
