97b55e9974ac8cd4a67e84160ed7ef58f94c06f2
Gregory Dardenne <gregory.dardenne@epitech.eu>
2024-10-08 16:25:41 +0200

    FIX : param id dans les methodes devient userID

--- lib/time_manager_web/controllers/user_controller.ex
+++ lib/time_manager_web/controllers/user_controller.ex
@@ -15,17 +15,17 @@ defmodule TimeManagerWeb.UserController do
     with {:ok, %User{} = user} <- Accounts.create_user(user_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", ~p"/api/users/#{user}")
+      |> put_resp_header("location", ~p"/api/users/#{user.id}")
       |> render(:show, user: user)
     end
   end
 
-  def show(conn, %{"id" => id}) do
+  def show(conn, %{"userID" => id}) do
     user = Accounts.get_user!(id)
     render(conn, :show, user: user)
   end
 
-  def update(conn, %{"id" => id, "user" => user_params}) do
+  def update(conn, %{"userID" => id, "user" => user_params}) do
     user = Accounts.get_user!(id)
 
     with {:ok, %User{} = user} <- Accounts.update_user(user, user_params) do
@@ -33,7 +33,7 @@ defmodule TimeManagerWeb.UserController do
     end
   end
 
-  def delete(conn, %{"id" => id}) do
+  def delete(conn, %{"userID" => id}) do
     user = Accounts.get_user!(id)
 
     with {:ok, %User{}} <- Accounts.delete_user(user) do
