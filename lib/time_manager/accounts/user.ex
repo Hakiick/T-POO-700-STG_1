196a5cf1c43935d2a267a55745d320d25f6ada8d
Gregory Dardenne <gregory.dardenne@epitech.eu>
2024-10-09 14:15:15 +0200

    rectification de la regex pour valider le format dans les users

--- lib/time_manager/accounts/user.ex
+++ lib/time_manager/accounts/user.ex
@@ -14,10 +14,10 @@ defmodule TimeManager.Accounts.User do
     user
     |> cast(attrs, [:username, :email])
     |> validate_required([:username, :email])
-    #|> validate_length(:username, min: 3, max: 20)
-    #|> validate_length(:email, max: 255)
-    # |> validate_format(:email, ~r/^[\w.!#$%&’*+/=?`{|}~^-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
-    #|> unique_constraint(:email)
-    #|> unique_constraint(:username)
+    |> validate_length(:username, min: 3, max: 20)
+    |> validate_length(:email, max: 255)
+    |> validate_format(:email, ~r/^[\w.!#$%&’*+\/=?`{|}~^-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+    |> unique_constraint(:email)
+    |> unique_constraint(:username)
   end
 end
