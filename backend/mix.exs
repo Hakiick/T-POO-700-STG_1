f5e596cf879c4c7dfa64f048fb076655b591e152
Hakiick <hakick@outlook.fr>
2024-10-10 13:28:52 +0200

    fix hackney dependency

--- backend/mix.exs
+++ backend/mix.exs
@@ -58,7 +58,8 @@ defmodule TimeManager.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:hackney, "~> 1.9"}
     ]
   end
 
