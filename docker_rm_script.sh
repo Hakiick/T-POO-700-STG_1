8b111bd2b0a05813f871fd3dae207cec4eb8fffb
Hakiick <hakick@outlook.fr>
2024-10-14 15:51:03 +0200

    add Cadvisor prometheus grafana

---
+++ docker_rm_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker rm time-manager_backend_1
+docker rm time-manager_db_1
+docker rm time-manager_frontend_1
+docker rm grafana
+docker rm zap
+docker rm prometheus
+docker rm cadvisor
