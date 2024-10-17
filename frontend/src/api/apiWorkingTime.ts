e03b3dbf4b95d75087a3f648518fa149a0a8769a
ratataque <ewanrimmely@outlook.fr>
2024-10-17 16:12:35 +0200

    working time added start and end

--- frontend/src/api/apiWorkingTime.ts
+++ frontend/src/api/apiWorkingTime.ts
@@ -3,12 +3,16 @@ import { AxiosResponse } from "axios";
 
 // Example: Fetch users from the API
 export const getWorkingTime = async (
-  user_id: number
+  user_id: number,
+  start: string,
+  end: string,
 ): Promise<AxiosResponse> => {
   try {
-    const response = await apiClient.get(`/workingtime/${user_id}`);
+    const response = await apiClient.get(
+      `/workingtime/${user_id}?start=${start}&end=${end}`,
+    );
     return response;
-  } catch (error : any) {
+  } catch (error: any) {
     if (error.response.status !== 404) {
       console.error("Error fetching workingtime:", error);
     }
