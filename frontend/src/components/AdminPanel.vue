d6ea319d534c6388fc37219806ffa88b3fef76e1
Gregory Dardenne <gregory.dardenne@epitech.eu>
2024-10-17 14:49:24 +0200

    End of working time modal vue

--- frontend/src/components/AdminPanel.vue
+++ frontend/src/components/AdminPanel.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
-import { createUser, deleteUser, getAllUser, updateUser } from '../api/apiUser.ts'
+import {
+  createUser,
+  deleteUser,
+  getAllUser,
+  updateUser,
+} from "../api/apiUser.ts";
 
 import {
   FlexRender,
@@ -10,9 +15,9 @@ import {
   getPaginationRowModel,
   getSortedRowModel,
   useVueTable,
-} from '@tanstack/vue-table'
+} from "@tanstack/vue-table";
 
-import { defineComponent, h } from 'vue'
+import { defineComponent, h } from "vue";
 import {
   Table,
   TableBody,
@@ -20,13 +25,13 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from './ui/table'
-import MainNav from './MainNav.vue'
-import Search from './Search.vue'
-import TeamSwitcher from './TeamSwitcher.vue'
-import UserNav from './UserNav.vue'
-import Button from './ui/button/Button.vue'
-import { Cross1Icon, Pencil2Icon, PlusIcon, TimerIcon } from '@radix-icons/vue'
+} from "./ui/table";
+import MainNav from "./MainNav.vue";
+import Search from "./Search.vue";
+import TeamSwitcher from "./TeamSwitcher.vue";
+import UserNav from "./UserNav.vue";
+import Button from "./ui/button/Button.vue";
+import { TrashIcon, Pencil2Icon, PlusIcon, TimerIcon } from "@radix-icons/vue";
 
 import {
   Dialog,
@@ -37,115 +42,175 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from './ui/dialog'
-import { Input } from './ui/input'
-import { Label } from './ui/label'
-import { User } from './store/userStore.ts'
-import WorkingTimeModal from './AdminPanelComponents/WorkingTimeModal.vue'
+} from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { User } from "./store/userStore.ts";
+import WorkingTimeModal from "./AdminPanelComponents/WorkingTimeModal.vue";
 </script>
 <script lang="ts">
 export default defineComponent({
   data() {
     return {
       users: [],
-      newUser: { id: -1, username: '', email:''}
-    }
-
+      newUser: { id: -1, username: "", email: "" },
+      actionUser: { id: -1, username: "", email: "" },
+      open: false,
+    };
   },
   methods: {
     async fetchAllUser() {
       const res = await getAllUser();
-      console.log(res);
       this.users = res;
-
     },
     async deleteElement(id: number) {
-      let user: User = this.users.filter((e:User) => e.id == id)[0];
+      let user: User = this.users.filter((e: User) => e.id == id)[0];
       if (await deleteUser(user)) {
         this.users = this.users.filter((e: User) => e.id != id);
       }
     },
     getElement(id: number) {
-      return this.users.filter(e => e.id == id)[0];
+      return this.users.filter((e) => e.id == id)[0];
     },
     async replaceElement(user: User) {
       const res = await updateUser(user);
-      console.log(res);
-
-
-      let users : User[] = [];
-      let index = this.users.findIndex(e => e.id == user.id);
-      this.users.slice(0, index).forEach(e => users.push(e));
+      let users: User[] = [];
+      let index = this.users.findIndex((e) => e.id == user.id);
+      this.users.slice(0, index).forEach((e) => users.push(e));
       users.push(user);
-      this.users.slice(index+1).forEach(e => users.push(e));
+      this.users.slice(index + 1).forEach((e) => users.push(e));
       this.users = users;
-      
     },
     async createElement() {
       try {
         const res = await createUser(this.newUser.username, this.newUser.email);
-        console.log(res.data);
-        let users : User[] = [];
+        let users: User[] = [];
         users.push(res.data);
         this.users.forEach((e: User) => users.push(e));
         this.users = users;
-
-      } catch(error) {
-        console.log("Error when creating element in front", error);
+      } catch (error) {
+        console.error("Error when creating element in front", error);
       }
-    }
+    },
   },
   computed: {
     user() {
       return false;
     },
-    
+
     columns() {
       const columnHelper = createColumnHelper<User>();
 
       return [
-        columnHelper.accessor('username', {
-          header: 'Username',
-          cell: ({ row }) => h('div', { class: 'capitalize usernames', 'data-id': row.getValue('id')}, row.getValue('username'))
+        columnHelper.accessor("username", {
+          header: "Username",
+          cell: ({ row }) =>
+            h(
+              "div",
+              { class: "capitalize usernames", "data-id": row.getValue("id") },
+              row.getValue("username")
+            ),
         }),
-        columnHelper.accessor('email', {
-          header: 'Email',
-          cell: ({ row }) => h('div', { class: 'lowercase emails', 'data-id': row.getValue('id')}, row.getValue('email')),
+        columnHelper.accessor("email", {
+          header: "Email",
+          cell: ({ row }) =>
+            h(
+              "div",
+              { class: "lowercase emails", "data-id": row.getValue("id") },
+              row.getValue("email")
+            ),
         }),
-        columnHelper.accessor('id', {
-          header: () => h('div', {class: 'text-right'}, 'Actions'),
-          cell: ({ row }) => h('div', { class: 'text-right'}, [
-            h(WorkingTimeModal, { user: { id:row.getValue('id'), username: row.getValue('username'), email: row.getValue('email')} }),
-            h(Dialog, { 'variant': 'outline' }, [ 
-              h(DialogTrigger, { asChild:true}, h(Button, {}, Pencil2Icon)),
-              h(DialogContent, {} , [
-                h(DialogHeader, {}, [
-                  h(DialogTitle, {}, 'Edit profile'),
-                  h(DialogDescription, {}, 'Modifier les informations du client ici')
-                ]),
-                h('div', { class: 'grid gap-4 py-4'}, [
-                  h('div', { class: 'grid grid-cols-4 items-center gap-4'}, [
-                    h(Label, {class: 'text-right'}, 'Email'),
-                    h(Input, { type:"email", placehorder:'Email', class:'input-email col-span-3','data-id': row.getValue('id') ,  modelValue:this.getElement(row.getValue('id')).email,  }),
+        columnHelper.accessor("id", {
+          header: () => h("div", { class: "text-right" }, "Actions"),
+          cell: ({ row }) =>
+            h("div", { class: "text-right" }, [
+              h(
+                Button,
+                {
+                  variant: "outline",
+                  onClick: () => {
+                    this.open = true;
+                    this.actionUser = {
+                      id: row.getValue("id"),
+                      username: row.getValue("username"),
+                      email: row.getValue("email"),
+                    };
+                  },
+                },
+                TimerIcon
+              ),
+              h(Dialog, { variant: "outline" }, [
+                h(DialogTrigger, { asChild: true }, h(Button, {}, Pencil2Icon)),
+                h(DialogContent, {}, [
+                  h(DialogHeader, {}, [
+                    h(DialogTitle, {}, "Edit profile"),
+                    h(
+                      DialogDescription,
+                      {},
+                      "Modifier les informations du client ici"
+                    ),
                   ]),
-                  h('div', { class: 'grid grid-cols-4 items-center gap-4'}, [
-                    h(Label, {class: 'text-right'}, 'Username'),
-                    h(Input, {type:'text', placeholder:'Username', class:'input-name col-span-3','data-id': row.getValue('id') , modelValue:this.getElement(row.getValue('id')).username})
-                  ])
+                  h("div", { class: "grid gap-4 py-4" }, [
+                    h("div", { class: "grid grid-cols-4 items-center gap-4" }, [
+                      h(Label, { class: "text-right" }, "Email"),
+                      h(Input, {
+                        type: "email",
+                        placehorder: "Email",
+                        class: "input-email col-span-3",
+                        "data-id": row.getValue("id"),
+                        modelValue: this.getElement(row.getValue("id")).email,
+                      }),
+                    ]),
+                    h("div", { class: "grid grid-cols-4 items-center gap-4" }, [
+                      h(Label, { class: "text-right" }, "Username"),
+                      h(Input, {
+                        type: "text",
+                        placeholder: "Username",
+                        class: "input-name col-span-3",
+                        "data-id": row.getValue("id"),
+                        modelValue: this.getElement(row.getValue("id"))
+                          .username,
+                      }),
+                    ]),
+                  ]),
+                  h(
+                    DialogFooter,
+                    {},
+                    h(
+                      DialogClose,
+                      { asChild: true },
+                      h(
+                        Button,
+                        {
+                          onClick: () => {
+                            this.replaceElement({
+                              id: row.getValue("id"),
+                              username: document.querySelector(
+                                `.input-name[data-id='${row.getValue("id")}']`
+                              ).value,
+                              email: document.querySelector(
+                                `.input-email[data-id='${row.getValue("id")}']`
+                              ).value,
+                            });
+                          },
+                        },
+                        "Save changes"
+                      )
+                    )
+                  ),
                 ]),
-                h(DialogFooter, {}, h(DialogClose, { asChild:true}, h(Button, { onClick: () => {
-                  this.replaceElement({
-                    id: row.getValue('id'), 
-                    username: document.querySelector(`.input-name[data-id='${row.getValue('id')}']`).value, 
-                    email: document.querySelector(`.input-email[data-id='${row.getValue('id')}']`).value
-                  })
-                }}, 'Save changes')))
-              ])
+              ]),
+              h(
+                Button,
+                {
+                  variant: "destructive",
+                  onClick: () => this.deleteElement(row.getValue("id")),
+                },
+                TrashIcon
+              ),
             ]),
-            h(Button, { 'variant': 'destructive', onClick: () => this.deleteElement(row.getValue('id'))}, Cross1Icon)
-          ])
-        })
-      ]
+        }),
+      ];
     },
     table() {
       return useVueTable({
@@ -153,22 +218,25 @@ export default defineComponent({
         columns: this.columns,
         getCoreRowModel: getCoreRowModel(),
         getPaginationRowModel: getPaginationRowModel(),
-      })
+      });
     },
   },
   mounted() {
-    this.fetchAllUser()
-  }
-})
-
-
-
-
-
+    this.fetchAllUser();
+  },
+});
 </script>
 
 <template>
-
+  <WorkingTimeModal
+    :open="open"
+    :user="actionUser"
+    @close="
+      () => {
+        this.open = false;
+      }
+    "
+  />
 
   <div class="h-full w-full">
     <div class="border-b">
@@ -185,79 +253,91 @@ export default defineComponent({
       <div class="m-5">
         <div class="flex justify-between text-center align-center">
           <h1 class="text-3xl bold uppercase">Admin panel</h1>
-          
 
           <Dialog>
-   <DialogTrigger as-child>
-     <Button variant="outline"><PlusIcon/></Button>
-   </DialogTrigger>
-   <DialogContent class="sm:max-w-[425px]">
-     <DialogHeader>
-       <DialogTitle>Add profile</DialogTitle>
-     </DialogHeader>
-     <div class="grid gap-4 py-4">
-       <div class="grid grid-cols-4 items-center gap-4">
-         <Label for="username" class="text-right">
-           Username
-         </Label>
-         <Input id="username" class="col-span-3" v-model="newUser.username"  />
-       </div>
-       <div class="grid grid-cols-4 items-center gap-4">
-         <Label for="email" class="text-right">
-           Email
-         </Label>
-         <Input id="email"  class="col-span-3" type="email" v-model="newUser.email" />
-       </div>
-     </div>
-     <DialogFooter>
-       <DialogClose>
-
-         <Button :onClick="createElement">
-           Save changes
-         </Button>
-       </DialogClose>
-     </DialogFooter>
-   </DialogContent>
- </Dialog>
+            <DialogTrigger as-child>
+              <Button variant="outline"><PlusIcon /></Button>
+            </DialogTrigger>
+            <DialogContent class="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Add profile</DialogTitle>
+              </DialogHeader>
+              <div class="grid gap-4 py-4">
+                <div class="grid grid-cols-4 items-center gap-4">
+                  <Label for="username" class="text-right"> Username </Label>
+                  <Input
+                    id="username"
+                    class="col-span-3"
+                    v-model="newUser.username"
+                  />
+                </div>
+                <div class="grid grid-cols-4 items-center gap-4">
+                  <Label for="email" class="text-right"> Email </Label>
+                  <Input
+                    id="email"
+                    class="col-span-3"
+                    type="email"
+                    v-model="newUser.email"
+                  />
+                </div>
+              </div>
+              <DialogFooter>
+                <DialogClose>
+                  <Button :onClick="createElement"> Save changes </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
 
         <Table>
-        <TableHeader>
-          <TableRow v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
-            <TableHead
-              v-for="header in headerGroup.headers" :key="header.id" :data-pinned="header.column.getIsPinned()"
+          <TableHeader>
+            <TableRow
+              v-for="headerGroup in table.getHeaderGroups()"
+              :key="headerGroup.id"
             >
-              <FlexRender v-if="!header.isPlaceholder" :render="header.column.columnDef.header" :props="header.getContext()" />
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <template v-if="table.getRowModel().rows?.length">
-            <template v-for="row in table.getRowModel().rows" :key="row.id">
-              <TableRow :data-state="row.getIsSelected() && 'selected'">
-                <TableCell
-                  v-for="cell in row.getVisibleCells()" :key="cell.id" :data-pinned="cell.column.getIsPinned()"
-                >
-                  <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
-                </TableCell>
-              </TableRow>
-              <TableRow v-if="row.getIsExpanded()">
-                <TableCell :colspan="row.getAllCells().length">
-                  {{ row.original }}
-                </TableCell>
-              </TableRow>
+              <TableHead
+                v-for="header in headerGroup.headers"
+                :key="header.id"
+                :data-pinned="header.column.getIsPinned()"
+              >
+                <FlexRender
+                  v-if="!header.isPlaceholder"
+                  :render="header.column.columnDef.header"
+                  :props="header.getContext()"
+                />
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <template v-if="table.getRowModel().rows?.length">
+              <template v-for="row in table.getRowModel().rows" :key="row.id">
+                <TableRow :data-state="row.getIsSelected() && 'selected'">
+                  <TableCell
+                    v-for="cell in row.getVisibleCells()"
+                    :key="cell.id"
+                    :data-pinned="cell.column.getIsPinned()"
+                  >
+                    <FlexRender
+                      :render="cell.column.columnDef.cell"
+                      :props="cell.getContext()"
+                    />
+                  </TableCell>
+                </TableRow>
+                <TableRow v-if="row.getIsExpanded()">
+                  <TableCell :colspan="row.getAllCells().length">
+                    {{ row.original }}
+                  </TableCell>
+                </TableRow>
+              </template>
             </template>
-          </template>
 
-          <TableRow v-else>
-            <TableCell
-              :colspan="columns.length"
-              class="h-24 text-center"
-            >
-              No results.
-            </TableCell>
-          </TableRow>
-        </TableBody>
+            <TableRow v-else>
+              <TableCell :colspan="columns.length" class="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          </TableBody>
         </Table>
       </div>
     </div>
--- frontend/src/components/AdminPanelComponents/WorkingTimeModal.vue
+++ frontend/src/components/AdminPanelComponents/WorkingTimeModal.vue
@@ -1,64 +1,219 @@
 <script setup lang="ts">
 import {
-    Dialog,
-    DialogTrigger,
-    DialogClose,
-    DialogContent,
-    DialogHeader,
-    DialogFooter
-
-} from "../ui/dialog"
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+} from "../ui/dialog";
 
 import { Button } from "../ui/button";
 import { User } from "../store/userStore.ts";
-import { onMounted, ref } from "vue";
-import { TimerIcon } from "@radix-icons/vue";
-import { getWorkingTimes } from "@/api/apiWorkingTime.ts";
+import { ref, watch, Ref, computed } from "vue";
+import {
+  createWorkingTime,
+  deleteWorkingTime,
+  getWorkingTimes,
+  updateWorkingTime,
+  WorkingTime,
+} from "@/api/apiWorkingTime.ts";
+
+import { PlusIcon, TrashIcon } from "@radix-icons/vue";
 
 const props = defineProps<{
-    user: User
-}>()
+  user: User;
+  open: boolean;
+}>();
 
-interface workingTime {
-    start: Date,
-    end: Date,
-    user_id: Number
-}
+const emit = defineEmits(["close"]);
+
+const workingTimes: Ref<WorkingTime[], WorkingTime[]> = ref([]);
 
 async function fetchWorkingTime() {
-    const res = await getWorkingTimes(props.user);
-    console.log(res);
+  workingTimes.value = [];
+  try {
+    let res: WorkingTime[] = await getWorkingTimes(props.user);
+    res = res.filter((i: WorkingTime) => new Date(i.end) - Date.now() > 0);
+    workingTimes.value = res;
+  } catch (error) {
+    // ignored
+  }
 }
 
-onMounted(fetchWorkingTime);
+const formattedDates = computed(() => {
+  return workingTimes.value.map((e: WorkingTime) => {
+    return {
+      id: e.id,
+      formattedDate: {
+        set: async function (val) {
+          let startDate = new Date(e.start);
+          let endDate = new Date(e.end);
+          let dates = val.split("-");
 
-const workingTime = ref([]);
+          startDate.setFullYear(dates[0]);
+          startDate.setMonth(dates[1] - 1);
+          startDate.setDate(dates[2]);
+          endDate.setFullYear(dates[0]);
+          endDate.setMonth(dates[1] - 1);
+          endDate.setDate(dates[2]);
+
+          e.start = startDate.toISOString();
+          e.end = endDate.toISOString();
+          await updateWorkingTime(e);
+        },
+        get: function () {
+          return formatDate(e.start);
+        },
+      },
+      formattedStartTime: {
+        set: async function (val) {
+          let date = new Date(e.start);
+          let times = val.split(":");
+          date.setHours(times[0]);
+          date.setMinutes(times[1]);
+          e.start = date.toISOString();
+          await updateWorkingTime(e);
+        },
+        get: function () {
+          return getFormatTime(e.start);
+        },
+      },
+      formattedEndTime: {
+        set: async function (val) {
+          let date = new Date(e.end);
+          let times = val.split(":");
+          date.setHours(times[0]);
+          date.setMinutes(times[1]);
+          e.end = date.toISOString();
+          await updateWorkingTime(e);
+        },
+        get: function () {
+          return getFormatTime(e.end);
+        },
+      },
+    };
+  });
+});
+
+function formatDate(dateString) {
+  let date = new Date(dateString);
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+}
+
+function getFormatTime(dateString) {
+  let date = new Date(dateString);
+  return `${date.getHours().toString().padStart(2, "0")}:${date
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}`;
+}
+
+async function newWorkingTime() {
+  try {
+    let res = await createWorkingTime(props.user, {
+      start: "2024-10-17T09:34:41.304Z",
+      end: "2024-10-17T09:34:41.304Z",
+    });
+
+    workingTimes.value.push(res.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function removeWorkingTime(id: Number) {
+  try {
+    let workingTimeToDelete = workingTimes.value.filter(
+      (e: WorkingTime) => e.id == id
+    )[0];
+    await deleteWorkingTime(workingTimeToDelete);
+    workingTimes.value = workingTimes.value.filter(
+      (e: WorkingTime) => e.id != id
+    );
+  } catch (error) {
+    console.error("front remove working time:", error);
+  }
+}
+
+watch(() => props.user?.id ?? -1, fetchWorkingTime);
 </script>
 <template>
-<Dialog>
-    <DialogTrigger as-child>
-      <Button variant="outline">
-        <TimerIcon/>
-      </Button>
-    </DialogTrigger>
-    <DialogContent class="sm:max-w-[425px] grid-rows-[auto_minmax(0,1fr)_auto] p-0 max-h-[90dvh]">
+  <Dialog
+    :open="props.open"
+    @update:open="
+      (val) => {
+        if (!val) emit('close');
+      }
+    "
+  >
+    <DialogContent
+      class="sm:max-w-[425px] grid-rows-[auto_minmax(0,4fr)_auto] p-0 max-h-[90dvh]"
+    >
       <DialogHeader class="p-6 pb-0">
-        <DialogTitle>Working time of {{ props.user?.username ?? '' }}</DialogTitle>
-        <DialogDescription></DialogDescription>
+        <DialogTitle
+          >Working time of {{ props.user?.username ?? "" }}</DialogTitle
+        >
+        <DialogDescription> </DialogDescription>
+        <Button @click="newWorkingTime"><PlusIcon /></Button>
       </DialogHeader>
       <div class="grid gap-4 py-4 overflow-y-auto px-6">
-        <div class="flex flex-col justify-between h-[300dvh]">
-          <p>
-            This is some placeholder content to show the scrolling behavior for modals. We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. When content becomes longer than the predefined max-height of modal, content will be cropped and scrollable within the modal.
-          </p>
+        <div class="flex flex-col">
+          <template
+            v-if="workingTimes.length != 0"
+            v-for="formattedDate in formattedDates"
+          >
+            <div class="flex border p-2">
+              <div class="grow">
+                <div class="flex justify-between w-5/6">
+                  <label>Date </label>
+                  <Input
+                    type="date"
+                    :value="formattedDate.formattedDate.get()"
+                    @input="
+                      formattedDate.formattedDate.set($event.target.value)
+                    "
+                    :min="formatDate(Date.now())"
+                  />
+                </div>
 
-          <p>This content should appear at the bottom after you scroll.</p>
+                <div class="flex justify-between w-5/6">
+                  <label>Start time </label>
+                  <Input
+                    type="time"
+                    :value="formattedDate.formattedStartTime.get()"
+                    @input="
+                      formattedDate.formattedStartTime.set($event.target.value)
+                    "
+                  ></Input>
+                </div>
+
+                <div class="flex justify-between w-5/6">
+                  <label>End time</label>
+                  <Input
+                    type="time"
+                    :value="formattedDate.formattedEndTime.get()"
+                    @input="
+                      formattedDate.formattedEndTime.set($event.target.value)
+                    "
+                  ></Input>
+                </div>
+              </div>
+              <div>
+                <Button
+                  variant="destructive"
+                  @click="removeWorkingTime(formattedDate.id)"
+                  ><TrashIcon
+                /></Button>
+              </div>
+            </div>
+          </template>
+          <div v-else>No working times registered for next days</div>
         </div>
       </div>
       <DialogFooter class="p-6 pt-0">
-        <Button type="submit">
-          Save changes
-        </Button>
+        <DialogClose>
+          <Button> Close </Button>
+        </DialogClose>
       </DialogFooter>
     </DialogContent>
   </Dialog>
