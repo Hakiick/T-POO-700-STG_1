8c103268e70432b61aa17c2b30be97a5acc5fd57
DorianC <dorian.cotteret@gmail.com>
2024-10-20 17:18:25 +0200

    Reste a controler le tableau et a faire les heures dynamites du bouton

--- frontend/src/components/ChartRange.vue
+++ frontend/src/components/ChartRange.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { type Ref, onMounted, ref } from 'vue';
 import { useUserStore } from './store/userStore';
 import { getUser } from '../api/apiUser';
 import { getWorkingTime } from '../api/apiWorkingTime';
-import { getClockFromUser } from '../api/apiClock';
+import { getClocksFromUser } from '../api/apiClock';
 import { getChartComponent, currentChartType, chartTypes } from '../manager/chartManager';
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
+import { RangeCalendar } from './ui/range-calendar';
+import { cn } from '../lib/utils';
+import { CalendarDate, DateFormatter, DateValue, getLocalTimeZone } from '@internationalized/date';
+import { CalendarIcon } from '@radix-icons/vue';
+
+import { getMonday, getSunday } from '../manager/DateUtils';
+
+import type { DateRange } from 'radix-vue';
+import { Button } from './ui/button';
+
+const df = new DateFormatter('fr-FR', {
+  dateStyle: 'medium',
+});
 
 const userStore = useUserStore();
 const user = ref(userStore.user);
@@ -13,30 +27,58 @@ const workingtime = ref<any>(null);
 const clockData = ref<any>(null);
 const workDays = ref<any[]>([]);
 const data = ref<any[]>([]);
+const workingDataTable = ref<any[]>([]);
+
+const value = ref({
+  start: new CalendarDate(
+    getMonday(new Date()).getFullYear(),
+    getMonday(new Date()).getMonth() + 1,
+    getMonday(new Date()).getDate()
+  ),
+  end: new CalendarDate(
+    getSunday(new Date()).getFullYear(),
+    getSunday(new Date()).getMonth() + 1,
+    getSunday(new Date()).getDate()
+  ),
+}) as Ref<DateRange>;
 
 onMounted(async () => {
   if (!user.value) {
     user.value = await getUser(1);
   }
-  // console.log(user.value);
   
-  // Récupération des données de working time et clock pour le mois en cours
-  const startOfMonth = new Date();
-  startOfMonth.setDate(1);
-  const endOfMonth = new Date(startOfMonth);
-  endOfMonth.setMonth(endOfMonth.getMonth() + 1); // Premier jour du mois suivant
-  endOfMonth.setDate(endOfMonth.getDate() - 1); // Dernier jour du mois courant
+  // Récupérer les données au chargement initial
+  await fetchData();
+});
 
-  workingtime.value = await getWorkingTime(user.value.id, startOfMonth.toISOString(), endOfMonth.toISOString());
-  clockData.value = await getClockFromUser(user.value.id);
+// Fonction pour appliquer la nouvelle plage de dates
+async function applyDateRange() {
+  await fetchData();
+}
 
-  console.log(workingtime.value, clockData.value);
+// Fonction pour récupérer les données de workingtime et clockData
+async function fetchData() {
+  const startDate = new Date(
+    value.value.start.year,
+    value.value.start.month - 1,
+    value.value.start.day
+  );
+  
+  // Ajoute 1 jour à la date de fin pour inclure toute la journée
+  const endDate = new Date(
+    value.value.end.year,
+    value.value.end.month - 1,
+    value.value.end.day + 1  // On ajoute 1 jour ici
+  );
 
-  // Transformation et fusion des données de working time et de clock
+  workingtime.value = await getWorkingTime(user.value.id, startDate.toISOString(), endDate.toISOString());
+  clockData.value = await getClocksFromUser(user.value.id, startDate.toISOString(), endDate.toISOString());
+
+  console.log(workingtime.value.data.data, clockData.value.data.data);
+
   const workingDays = aggregateWorkingTime(workingtime.value.data.data);
   const clockDays = calculateWork(clockData.value.data.data);
 
-  // Fusion des jours travaillés avec les données des heures planifiées et réelles
   workDays.value = workingDays.map(workDay => {
     const clockDay = clockDays.find(day => day.name === workDay.name);
     const realTime = clockDay ? clockDay.real : 0;
@@ -48,15 +90,40 @@ onMounted(async () => {
     };
   });
 
-  // Préparation des données pour les graphiques
   data.value = workDays.value.map(day => ({
     name: day.name,
     planned: day.planned,
     real: day.real,
   }));
-});
 
-// Fonction pour agréger les données de working time par date
+  workingDataTable.value = createDataTable(workingtime.value.data.data, clockData.value.data.data);
+}
+
+function createDataTable(workingTimeEntries: any[], clockEntries: any[]) {
+  console.log(workingTimeEntries, clockEntries);
+
+  // Créer un tableau vide pour stocker les résultats
+  let dataTable = [];
+
+  // Parcourir les entrées du temps de travail
+  workingTimeEntries.forEach((workingTime) => {
+    // Rechercher les entrées de pointage correspondant à la date de travail (start et end)
+    const clockStart = clockEntries.find((clock) => clock.status === true && new Date(clock.time).toDateString() === new Date(workingTime.start).toDateString());
+    const clockEnd = clockEntries.find((clock) => clock.status === false && new Date(clock.time).toDateString() === new Date(workingTime.end).toDateString());
+
+    // Ajouter une ligne avec les données correspondantes
+    dataTable.push({
+      date: new Date(workingTime.start).toLocaleDateString(), // Format de la date
+      startTime: new Date(workingTime.start).toLocaleTimeString(), // Heure de début
+      endTime: new Date(workingTime.end).toLocaleTimeString(), // Heure de fin
+      clockStart: clockStart ? new Date(clockStart.time).toLocaleTimeString() : 'N/A', // Pointage début
+      clockEnd: clockEnd ? new Date(clockEnd.time).toLocaleTimeString() : 'N/A' // Pointage fin
+    });
+  });
+
+  return dataTable;
+}
+
 function aggregateWorkingTime(workingTimeEntries: any[]) {
   const aggregated: Record<string, { planned: number }> = {};
 
@@ -70,52 +137,41 @@ function aggregateWorkingTime(workingTimeEntries: any[]) {
     aggregated[formattedDate].planned += durationNumeric;
   });
 
-  // Convertir le Record en tableau
   return Object.keys(aggregated).map(name => ({
     name,
     planned: aggregated[name].planned,
   }));
 }
 
-// Fonction pour calculer le temps travaillé entre un statut true et false pour chaque journée
 function calculateWork(clockEntries: any[]) {
-  // S'assurer que clockEntries est bien un tableau
-  if (!Array.isArray(clockEntries)) {
-    return [];
-  }
-  // console.log('Clock Entries:', clockData.value);
+  if (!Array.isArray(clockEntries)) return [];
+
   const workByDate: Record<string, { real: number }> = {};
   let startTime: string | null = null;
 
   clockEntries.forEach(entry => {
     const formattedDate = formatDate(entry.time);
 
-    // Si c'est une entrée 'true', on enregistre le début
     if (entry.status) {
       startTime = entry.time;
-    } 
-    // Si c'est une entrée 'false', on calcule la durée entre start et end
-    else if (startTime) {
+    } else if (startTime) {
       const { durationNumeric } = calculateDuration(startTime, entry.time);
-      
-      // Ajout de la durée à la date correspondante
+
       if (!workByDate[formattedDate]) {
         workByDate[formattedDate] = { real: 0 };
       }
 
       workByDate[formattedDate].real += durationNumeric;
-      startTime = null; // Réinitialiser pour le prochain cycle
+      startTime = null;
     }
   });
 
-  // Convertir le Record en tableau
   return Object.keys(workByDate).map(date => ({
     name: date,
     real: workByDate[date].real,
   }));
 }
 
-// Fonction pour calculer la durée en heures et minutes entre deux dates
 function calculateDuration(start: string, end: string) {
   const startDate = new Date(start);
   const endDate = new Date(end);
@@ -131,7 +187,6 @@ function calculateDuration(start: string, end: string) {
   };
 }
 
-// Fonction pour formater une date au format dd/MM
 function formatDate(dateString: string) {
   const date = new Date(dateString);
   const day = String(date.getDate()).padStart(2, '0');
@@ -139,24 +194,111 @@ function formatDate(dateString: string) {
 
   return `${day}/${month}`;
 }
+
+function formatHours(hours: number) {
+  const h = Math.floor(hours);
+  const m = Math.round((hours - h) * 60);
+  return `${h}h ${m}m`;
+}
 </script>
 
 <template>
-  <div>
-    <div class="flex items-center justify-center space-x-4 mb-10">
-    <button
-      v-for="type in chartTypes"
-      :key="type"
-      @click="currentChartType = type"
-      class="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
-    >
-      {{ type }}
-    </button>
-  </div>
+  <Tabs default-value="monthly" class="space-y-4 h-full w-full">
+    <TabsContent value="monthly" class="space-y-4 h-full w-full">
+      <div class="grid gap-4 grid-cols-1 lg:grid-cols-10 h-full w-full mt-7">
+        
+        <!-- Section graphique - 70% largeur à gauche -->
+        <div class="col-span-1 lg:col-span-6 space-y-4 h-full w-full">
+          <div class="h-full w-full">
+            <div class="flex items-center justify-center space-x-4 mb-10">
+              <button
+                v-for="type in chartTypes"
+                :key="type"
+                @click="currentChartType = type"
+                class="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              >
+                {{ type }}
+              </button>
+            </div>
 
-    <component
-      :is="getChartComponent(currentChartType, data, 'name', ['planned', 'real'], ['#3498db', '#2ecc71']).component"
-      v-bind="getChartComponent(currentChartType, data, 'name', ['planned', 'real'], ['#3498db', '#2ecc71']).props"
-    />
-  </div>
+            <!-- Composant graphique dynamique -->
+            <component
+              :is="getChartComponent(currentChartType, data, 'name', ['planned', 'real'], ['#3498db', '#2ecc71']).component"
+              v-bind="getChartComponent(currentChartType, data, 'name', ['planned', 'real'], ['#3498db', '#2ecc71']).props"
+            />
+          </div>
+        </div>
+
+        <!-- Section emploi du temps - 30% largeur à droite -->
+        <div class="col-span-1 lg:col-span-4 h-full w-full space-y-4">
+          <!-- Calendrier avec bouton d'application -->
+          <div class="text-center mb-3">
+            <Popover>
+              <PopoverTrigger as-child>
+                <Button
+                  variant="outline"
+                  :class="cn(
+                    'w-full justify-start text-left font-normal',
+                    !value && 'text-muted-foreground',
+                  )"
+                >
+                  <CalendarIcon class="mr-2 h-4 w-4" />
+                  <template v-if="value.start">
+                    <template v-if="value.end">
+                      {{ df.format(value.start.toDate(getLocalTimeZone())) }} - {{ df.format(value.end.toDate(getLocalTimeZone())) }}
+                    </template>
+                    <template v-else>
+                      {{ df.format(value.start.toDate(getLocalTimeZone())) }}
+                    </template>
+                  </template>
+                  <template v-else>
+                    Pick a date
+                  </template>
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent class="w-auto p-0">
+                <RangeCalendar
+                  v-model="value"
+                  initial-focus
+                  :number-of-months="2"
+                  @update:start-value="(startDate) => value.start = startDate"
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+
+          <!-- Bouton pour appliquer la plage de dates -->
+          <div class="text-center mb-3">
+            <Button @click="applyDateRange" class="bg-green-500 hover:bg-green-600 text-white">
+              Appliquer
+            </Button>
+          </div>
+
+          <!-- Emploi du temps -->
+          <div class="text-center mb-3">
+            <table class="w-full">
+              <thead>
+                <tr>
+                  <th class="px-4 py-2 text-left">Date</th>
+                  <th class="px-4 py-2 text-left">Heure de début</th>
+                  <th class="px-4 py-2 text-left">Heure de fin</th>
+                  <th class="px-4 py-2 text-left">Pointage de début</th>
+                  <th class="px-4 py-2 text-left">Pointage de fin</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(row, index) in workingDataTable" :key="index">
+                  <td class="border px-4 py-2">{{ row.date }}</td>
+                  <td class="border px-4 py-2">{{ row.startTime }}</td>
+                  <td class="border px-4 py-2">{{ row.endTime }}</td>
+                  <td class="border px-4 py-2">{{ row.clockStart }}</td>
+                  <td class="border px-4 py-2">{{ row.clockEnd }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </TabsContent>
+  </Tabs>
 </template>
--- frontend/src/components/Template.vue
+++ frontend/src/components/Template.vue
@@ -10,30 +10,22 @@ defineOptions({
 // ============================
 // Imports des composants locaux et utilitaires
 // ============================
-import Day from './Day.vue';
 import ChartRange from './ChartRange.vue';
 import MainNav from './MainNav.vue';
 import Search from './Search.vue';
 import TeamSwitcher from './TeamSwitcher.vue';
 import UserNav from './UserNav.vue';
-import WorkingTime from './WorkingTime.vue';
 
 // ============================
 // Imports des composants UI
 // ============================
-import { Calendar } from './ui/calendar';
-import { Button } from './ui/button';
-import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
 import { Switch } from './ui/switch';
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs';
-import { CalendarIcon } from '@radix-icons/vue';
+import { Tabs, TabsContent } from './ui/tabs';
 
 // ============================
 // Imports des bibliothèques utilitaires
 // ============================
-import { cn } from '../lib/utils';
-import { CalendarDate, DateFormatter, DateValue, getLocalTimeZone } from '@internationalized/date';
 import moment from 'moment';
 import { onMounted, ref, watch } from 'vue';
 import { AxiosResponse } from 'axios';
@@ -42,7 +34,6 @@ import { AxiosResponse } from 'axios';
 // Imports des API
 // ============================
 import { getUser } from '../api/apiUser';
-import { getWorkingTime } from '../api/apiWorkingTime';
 import { createClock, getClockFromUser, getClocksFromUser } from '../api/apiClock';
 
 // ============================
@@ -65,8 +56,6 @@ const last_clock = ref(null);
 const last_clock_value = ref(false);
 const clock_diable = ref(false);
 const current_time = ref("");
-const workingtime = ref<any>(null);
-const clockDataDay = ref<any>(null);
 const clockDataWeek = ref<any>(null);
 const clockDataMonth = ref<any>(null);
 const workedHoursToday = ref<string | null>(null);
@@ -77,8 +66,6 @@ const departureTime = ref<string | null>(null);
 // ============================
 // Variables liées à la date sélectionnée
 // ============================
-const oneDateValue = ref(null);
-const oneDayChart = ref<any>(null);
 
 const df = (date: Date) => moment(date).format('DD-MM-YYYY');
 
@@ -207,78 +194,6 @@ const handleChangeClock = async (checked: boolean) => {
     { time: moment.utc().format('YYYY-MM-DDTHH:mm:ss[Z]'), status: last_clock_value.value }, user.value.id
   );
 }
-
-
-
-
-watch(oneDateValue, (newValue) => {
-  if (newValue) {
-    // Extraire les détails de la date
-    const year = newValue.year;
-    const month = String(newValue.month).padStart(2, '0');
-    const day = String(newValue.day).padStart(2, '0');
-
-    const userData = user.value.id;
-
-    // Formater la date pour obtenir un format spécifique
-    const formattedDate = `${year}-${month}-${day}`;
-    console.log('Formatted Date:', formattedDate);
-
-    // Exemple: Supposons que vous avez une fonction qui récupère les heures
-    // oneDayChart.value =  getHoursForDate(formattedDate, userData);
-    getHoursForDate(formattedDate, userData).then((data) => {
-      oneDayChart.value = data;
-      // console.log("asdf", oneDayChart.value[0]);
-      // console.log("asdf", data);
-    });
-
-    // Formater les heures et mettre à jour oneDayChart
-  }
-});
-
-async function getHoursForDate(date, userData) {
-
-  const startOfDay = moment(date).startOf('day').toISOString();
-  const endOfDay = moment(date).endOf('day').toISOString();
-
-  const workDay = await getClocksFromUser(userData, startOfDay, endOfDay);
-  console.log(workDay.data);
-  return workDay.data.data;
-}
-
-
-// ============================
-// Jeu d'essai: Calcul des heures travaillées pour des dates en août
-// ============================
-async function testCalculateWorkedHours() {
-  const userId = 1; // ID de l'utilisateur pour le test
-
-  // Test pour une journée en août
-  const startDateDay = moment('2024-09-14').startOf('day').toISOString();
-  const endDateDay = moment('2024-09-14').endOf('day').toISOString();
-  const workedHoursDay = await calculateWorkedHours(userId, startDateDay, endDateDay);
-  //console.log(`Worked hours on 2024-09-15: ${workedHoursDay}`);
-
-  // Test pour une semaine en août
-  const startDateWeek = moment('2024-09-12').startOf('isoWeek').toISOString();
-  const endDateWeek = moment('2024-09-18').endOf('isoWeek').toISOString();
-  const workedHoursWeek = await calculateWorkedHours(userId, startDateWeek, endDateWeek);
-  //console.log(`Worked hours from 2024-09-12 to 2024-09-18: ${workedHoursWeek}`);
-
-  // Test pour le mois d'août
-  const startDateMonth = moment('2024-09-01').startOf('month').toISOString();
-  const endDateMonth = moment('2024-09-30').endOf('month').toISOString();
-  const workedHoursMonth = await calculateWorkedHours(userId, startDateMonth, endDateMonth);
-  //console.log(`Worked hours in September 2024: ${workedHoursMonth}`);
-}
-
-// Décommenter pour exécuter le jeu d'essai
-// testCalculateWorkedHours();
-
-
-
-
-
 </script>
 
 
@@ -369,126 +284,14 @@ async function testCalculateWorkedHours() {
         </div>
       </div>
 
-      <Tabs default-value="day" class="space-y-4 h-full w-full">
-
-        <TabsList class="flex items-center justify-center text-center lg:w-[66vw]">
-          <TabsTrigger value="day">
-            Jour
-          </TabsTrigger>
-          <TabsTrigger value="weekly">
-            Semaine
-          </TabsTrigger>
-          <TabsTrigger value="monthly">
-            Mois
-          </TabsTrigger>
-        </TabsList>
-
-        <!-- Day -->
-        <TabsContent value="day" class="space-y-4 h-full w-full">
-          <div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-10 h-full w-full mt-7">
-            <!-- Graphique par jour -->
-            <div class="col-span-1 lg:col-span-7 space-y-4 h-full w-full">
-              <Card class="h-full w-full">
-                <CardContent class="bottom-p-0 h-full">
-                  <!-- Passer la date sélectionnée à Day -->
-                  <Day :oneDayChart="oneDayChart" />
-                </CardContent>
-              </Card>
-            </div>
-
-            <!-- Sélecteur de date et emploi du temps -->
-            <div class="col-span-1 lg:col-span-3 h-full w-full lg:top-[-64px] lg:relative">
-              <div class="flex items-center justify-center text-center">
-                <Popover>
-                  <PopoverTrigger as-child>
-                    <Button variant="outline" :class="cn(
-                      'w-[280px] justify-start text-left font-normal lg:mb-4',
-                      !oneDateValue && 'text-muted-foreground',
-                    )">
-                      <CalendarIcon class="mr-2 h-4 w-4" />
-                      {{ oneDateValue ? df(oneDateValue.toDate(getLocalTimeZone())) : "Choisir une date" }}
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent class="w-auto p-0">
-                    <!-- Utiliser v-model pour récupérer la date sélectionnée -->
-                    <Calendar v-model="oneDateValue" />
-                  </PopoverContent>
-                  <p>{{ oneDateValue ? df(oneDateValue.toDate(getLocalTimeZone())) : '' }}</p>
-                </Popover>
-              </div>
-
-              <!-- Emploi du temps avec la date sélectionnée -->
-              <Card class="h-full w-full mt-3">
-                <CardHeader>
-                  <CardTitle>Emploi du temps</CardTitle>
-                </CardHeader>
-                <CardContent class="bottom-p-0 h-full">
-                  <!-- Passer la date sélectionnée à WorkingTime -->
-
-                </CardContent>
-              </Card>
-            </div>
-          </div>
-        </TabsContent>
-
-        <!-- Week -->
-        <TabsContent value="weekly" class="space-y-4 h-full w-full">
-          <div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-10 h-full w-full mt-7">
-            <div class="col-span-1 lg:col-span-7 space-y-4 h-full w-full">
-              <Card class="h-full w-full">
-                <CardHeader>
-                  <CardTitle>Graphique en semaine</CardTitle>
-                </CardHeader>
-                <CardContent class="bottom-p-0 h-full" v-if="user">
-                  <ChartRange :user="user" />
-                </CardContent>
-              </Card>
-            </div>
-            <div class="col-span-1 lg:col-span-3 h-full w-full">
-              <div class="flex items-center justify-center text-center">
-
-              </div>
-              <Card class="h-full w-full mt-3">
-                <CardHeader>
-                  <CardTitle>Emploi du temps</CardTitle>
-                </CardHeader>
-                <CardContent class="bottom-p-0 h-full" v-if="user">
-                  <WorkingTime :user="user" />
-                </CardContent>
-              </Card>
-            </div>
-          </div>
-        </TabsContent>
-
-        <!-- Monthly -->
+      <hr>
+      
+      <Tabs default-value="monthly" class="space-y-4 h-full w-full">
         <TabsContent value="monthly" class="space-y-4 h-full w-full">
-          <div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-10 h-full w-full mt-7">
-            <div class="col-span-1 lg:col-span-7 space-y-4 h-full w-full">
-              <Card class="h-full w-full">
-                <CardHeader>
-                  <CardTitle>Graphique en mois</CardTitle>
-                </CardHeader>
-                <CardContent class="bottom-p-0 h-full" v-if="user">
-                  <ChartRange :user="user" />
-                </CardContent>
-              </Card>
-            </div>
-            <div class="col-span-1 lg:col-span-3 h-full w-full">
-              <div class="flex items-center justify-center text-center">
-
-              </div>
-              <Card class="h-full w-full mt-3">
-                <CardHeader>
-                  <CardTitle>Emploi du temps</CardTitle>
-                </CardHeader>
-                <CardContent class="bottom-p-0 h-full" v-if="user">
-                  <WorkingTime :user="user" />
-                </CardContent>
-              </Card>
-            </div>
-          </div>
+          <CardContent class="bottom-p-0 h-full" v-if="user">
+            <ChartRange :user="user" />
+          </CardContent>
         </TabsContent>
-
       </Tabs>
     </div>
   </div>
