99cce77a7504e4bf68b568ce693160ab8cf92507
ratataque <ewanrimmely@outlook.fr>
2024-10-21 13:22:34 +0200

    added access on login

--- backend/lib/time_manager_web/user_auth.ex
+++ backend/lib/time_manager_web/user_auth.ex
@@ -19,11 +19,19 @@ defmodule TimeManagerWeb.UserAuth do
   if you are not using LiveView.
   """
   def log_in_user(conn, user) do
-    {:ok, jwt, _full_claims} = Guardian.encode_and_sign(user, %{}, token_type: "refresh")
+    {:ok, refresh_token, _full_claims} =
+      Guardian.encode_and_sign(user, %{}, token_type: "refresh")
+
+    {:ok, acces_token, _full_claims} =
+      Guardian.encode_and_sign(user, %{}, token_type: "acces_token")
 
     conn
     # |> put_resp_cookie("auth_token", jwt, http_only: true)
-    |> json(%{refresh_token: jwt, message: "User logged in successfully."})
+    |> json(%{
+      refresh_token: refresh_token,
+      acces_token: acces_token,
+      message: "User logged in successfully."
+    })
   end
 
   def refresh_token(conn) do
