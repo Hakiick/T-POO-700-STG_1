68abc31bfc08d4ebe76fd0e7a4b43a25c2b8ece1
ratataque <ewanrimmely@outlook.fr>
2024-10-26 19:52:17 +0200

    create route for creating user from manager

--- backend/lib/time_manager_web/controllers/user_registration_controller.ex
+++ backend/lib/time_manager_web/controllers/user_registration_controller.ex
@@ -2,7 +2,7 @@ defmodule TimeManagerWeb.UserRegistrationController do
   use TimeManagerWeb, :controller
 
   alias TimeManager.Accounts
-  alias TimeManagerWeb.UserAuth
+  # alias TimeManagerWeb.UserAuth
 
   def create(conn, %{"user" => user_params}) do
     # add the role to the user_params
@@ -20,8 +20,42 @@ defmodule TimeManagerWeb.UserRegistrationController do
 
         conn
         |> put_status(:created)
-        # |> json(%{ok: "user created"})
-        |> UserAuth.log_in_user(user)
+        |> json(%{ok: "user created"})
+
+      # |> UserAuth.log_in_user(user)
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        # conn
+        # |> put_status(:bad_request)
+
+        render(conn, :show, changeset: changeset)
+        # |> put_status(:bad_request)
+        # |> json(%{error: changeset.errors.full_messages})
+    end
+  end
+
+  def create_by_manager(conn, %{"user" => user_params}) do
+    # add the role to the user_params
+    user_params = Map.put(user_params, "role", "user")
+    # add a random uid as password in the user_params
+    user_params =
+      Map.put(user_params, "password", :crypto.strong_rand_bytes(16) |> Base.encode64())
+
+    # IO.inspect(user_params)
+
+    case Accounts.register_user(user_params) do
+      {:ok, user} ->
+        {:ok, _} =
+          Accounts.deliver_user_reset_password_instructions(
+            user,
+            &url(~p"/users/reset_password/#{&1}")
+          )
+
+        conn
+        |> put_status(:created)
+        |> json(%{ok: "user created"})
+
+      # |> UserAuth.log_in_user(user)
 
       {:error, %Ecto.Changeset{} = changeset} ->
         # conn
